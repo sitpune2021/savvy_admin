7aff76fa930db422b3c38d1c0c563aaf2f93f8bd
Avantika1327 <avantika.kawale@gmail.com>
2025-04-15 15:23:10 +0530

    minor changes

--- database/migrations/2025_04_15_061618_update_column_type_in_maintenance_table.php
+++ database/migrations/2025_04_15_061618_update_column_type_in_maintenance_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('maintenances', function (Blueprint $table) {
-            $table->json('image')->nullable()->change();
+            $table->text('image')->nullable()->change();
         });
     }
 
