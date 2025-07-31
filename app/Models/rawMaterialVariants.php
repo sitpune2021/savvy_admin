2241bd08e75e62cd7d0bd0228be1ae4b12cc1b6a
Avantika1327 <avantika.kawale@gmail.com>
2025-07-31 18:34:29 +0530

    label logic

--- app/Models/rawMaterialVariants.php
+++ app/Models/rawMaterialVariants.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\rawStockTransactions;
+use App\Models\rawMaterial;
 
 class rawMaterialVariants extends Model
 {
