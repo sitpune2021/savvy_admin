826d91b1be2f142620e1e2b59d3d3524818c0f52
Shekhar <shekharmudmesitsolution@gmail.com>
2026-05-29 16:11:48 +0530

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         DB::statement("SET time_zone = '+05:30'");
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,20 +12,22 @@ use Database\Seeders\AppVersionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
+    
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::create([
-        //     'name' => 'admin',  // Name of the user
-        //     'email' => 'admin@gmail.com',  // Email of the user
-        //     'role' => 'admin',  // Email of the user
-        //     'password' => Hash::make('Test@123'),  // Password, hashed
-        // ]);
+        User::create([
+            'name' => 'admin',  // Name of the user
+            'email' => 'admin@gmail.com',  // Email of the user
+            'role' => 'admin',  // Email of the user
+            'password' => Hash::make('Test@123'),  // Password, hashed
+        ]);
 
         $this->call(AppVersionSeeder::class);
         $this->call(MaterialSeeder::class);
         $this->call(MaterialVariantSeeder::class);
     }
+
 }
